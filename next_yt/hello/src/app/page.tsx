32619c08b5f9efa06a6c07b53f828d37866573b2
Mahmud <117104429+MAHMUD1223@users.noreply.github.com>
2024-01-26 01:21:00 +0000

    Refactor layout and page components

--- next_yt/hello/src/app/page.tsx
+++ next_yt/hello/src/app/page.tsx
@@ -1,5 +1,3 @@
-export default function App(){
-  return (
-  <h1 align="center">Hello </h1>
-  );
+export default function App() {
+  return <h1 align='center'>Hello </h1>;
 }
--- next_yt/hello/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ next_yt/hello/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,7 +1,11 @@
-export default function ProductDetails({params}:{params:{productId : string, reviewId: string}}){
-    return (
-        <h1>
-        review of {params.reviewId} product {params.productId}
-        </h1>
-    )
+export default function ProductDetails({
+  params,
+}: {
+  params: { productId: string; reviewId: string };
+}) {
+  return (
+    <h1>
+      review of {params.reviewId} product {params.productId}
+    </h1>
+  );
 }
