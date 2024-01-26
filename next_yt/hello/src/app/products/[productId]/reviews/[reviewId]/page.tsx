84ac7392e780d264558b7329d752f17d60bcc878
Mahmud <117104429+MAHMUD1223@users.noreply.github.com>
2024-01-26 01:18:24 +0000

    Refactor code to improve performance and readability

---
+++ next_yt/hello/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,7 @@
+export default function ProductDetails({params}:{params:{productId : string, reviewId: string}}){
+    return (
+        <h1>
+        review of {params.reviewId} product {params.productId}
+        </h1>
+    )
+}
